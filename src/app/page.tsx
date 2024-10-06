0415c56fc7af090cf093102802f4e932b8dfcf45
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-06 09:50:09 +0630

    feat: add reusable hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,10 @@
-import Image from "next/image";
+import Hero from "@/components/hero";
 import homeImg from "/public/home.jpg";
 
-console.log(homeImg);
 export default function Home() {
   return (
     <div>
-      Home page
-      {/* <div className="absolute -z-10 inset-0">
-        <Image
-          src={homeImg}
-          alt="car factory"
-          fill
-          style={{ objectFit: "cover" }}
-        />
-      </div> */}
-      {/* image size is equal to imported image  */}
-      {/* <div>
-        <Image src={homeImg} alt="car factory" />
-        <div>Buy our product</div>
-      </div> */}
-      {/* image size will be given width and height */}
-      {/* <div>
-        <Image src={homeImg} alt="car factory" width={400} height={400} />
-        <div>Buy our product</div>
-      </div> */}
-      {/* image will expand to fill up the parent element */}
-      <div>
-        <Image src={homeImg} alt="car factory" fill />
-        <div>Buy our product</div>
-      </div>
+      <Hero imgData={homeImg} alt="car factory" title="Buy our product" />
     </div>
   );
 }
